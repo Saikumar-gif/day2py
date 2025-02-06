14932f0b8f40f48c698bb41e6094495f1d205630
Saikumar-gif <saikumarjadala627@gmail.com>
2025-02-06 11:54:08 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,53 @@
+print(1+2)#addition
+print(1-2)#subtraction
+print(1/2)#division
+print(1*2)#multiplication
+print(1**2)#exponent
+print(1%2)#to print remainder
+
+print(2//5)#to print integer value
+
+# To print variable values
+
+a=2 #variable
+b=4#variable
+
+print(a+b)#print variables
+
+
+print(a-b)
+print(a*b)
+print(a**b)
+print(a/b)
+print(a%b)
+
+
+
+
+
+str1="mynameissai"
+print(str1)
+print(len(str1))
+print(str1[0])
+print(str1[10])
+
+
+#Negativ indexing
+
+print(str1[-1])
+print(str1[-9])
+
+
+
+#slicing
+
+print(str1[2:9])
+print(str1[0:9])
+print(str1[0:])
+print(str1[:13])
+print(str1[-8:-3])
+print(str1[-2:-9])
+print(str1[-2:-8:-1])
+print(str1[0:14:2])
+
+
